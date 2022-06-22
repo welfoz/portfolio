2f1a9f7085b4286df9b7f50a1d7ed864f7b045ca
Fabien MICHEL <fabien@michel.com>
2022-06-22 22:00:13 +0200

    lint clear

--- components/rsBar.js
+++ components/rsBar.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import gitub from "../public/githubWhiteIcon.png" 
 import linkedin from "../public/linkedinIcon.png" 
 import twitter from "../public/twitterIcon.png" 
-export default function rsBar() {
+export default function RsBar() {
     return(
         <div className='fixed bottom-0 right-7 mix-blend-exclusion z-30'>
     <nav className="flex flex-col items-center space-y-1">
@@ -19,7 +19,7 @@ export default function rsBar() {
 function Icon({link}) {
     return(
         <div className='hover:scale-110 hover:-translate-y-1 transition duration-100'>
-        <Image src={link} height="40" width="40"/>
+        <Image src={link} height="40" width="40" alt='socialIcon'/>
         </div>
     );
 }
