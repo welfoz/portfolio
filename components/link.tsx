9a37f079480bebb25bd7844cf6ca4eebf158c8dc
Fabien MICHEL <fabien@michel.com>
2022-06-26 01:06:05 +0200

    align icons

--- components/link.tsx
+++ components/link.tsx
@@ -4,7 +4,9 @@ export default function IconLink(props: {src : StaticImageData, link: string}) {
     return(
         <div className='hover:scale-110 hover:-translate-y-1 transition duration-100 w-fit'>
             <a href={props.link}>
-                <Image src={props.src} height="35" width="35" alt='socialIcon'/>
+                <div className='w-8 h-8 relative'>
+                <Image layout='fill' objectFit='contain' src={props.src} alt='socialIcon' objectPosition="50% 0%"/>
+                </div>
             </a>
         </div>
     );
--- components/links.tsx
+++ components/links.tsx
@@ -3,7 +3,7 @@ import gitub from "../public/githubIcon.png"
 
 export default function Links() {
     return(
-        <div className="w-full flex justify-end">
+        <div className="w-full flex justify-end items-start">
              <div className="mr-2">
                 <IconLink src={gitub} link="" />
              </div>
